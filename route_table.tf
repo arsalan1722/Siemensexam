50ad6d75f37eb5ebcdf338e81f0ff239b73828c2
arsalan <your-email@example.com>
2025-03-19 15:09:33 +0530

    Added route_table.tf

---
+++ route_table.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "private_rt" {
+  vpc_id = data.aws_vpc.selected.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = data.aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
