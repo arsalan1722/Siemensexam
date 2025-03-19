379311e7558dd7853e7343b4389c09359ec9d93c
arsalan <your-email@example.com>
2025-03-19 15:11:19 +0530

    Added security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lambda-security-group"
+  }
+}
